d1acc4ed203ca8a7e7fddb8a607a4fda933d8702
ErezBiren <biran330@gmail.com>
2021-04-05 23:22:34 +0300

    Updates

---
+++ precache-manifest.aaaf7c37d574754962a5a22d12f3a5f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bf2a98ed3e8158f94cf1154ade5a9d3",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "9aa4ce95df415523505a",
+    "url": "/calculator/static/css/2.2d5861c9.chunk.css"
+  },
+  {
+    "revision": "9466f09da51a22f6e1bb",
+    "url": "/calculator/static/css/main.37131298.chunk.css"
+  },
+  {
+    "revision": "9aa4ce95df415523505a",
+    "url": "/calculator/static/js/2.c5636010.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/calculator/static/js/2.c5636010.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9466f09da51a22f6e1bb",
+    "url": "/calculator/static/js/main.3156bfa4.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
